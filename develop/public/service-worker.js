88941a45a3a7bb05b37eb730c56c05b75b74f4d2
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-03-20 20:21:10 -0400

    moved folders

--- develop/public/service-worker.js
+++ develop/public/service-worker.js
@@ -2,6 +2,16 @@ const APP_PREFIX = 'MoneyTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
+const FILES_TO_CACHE = [
+    "./models/transaction.js",
+    "./public/css/style.css",
+    "./public/js/idb.js",
+    "./public/js/index.js",
+    "./public/js/index.html",
+    "./routes/api.js",
+    "./server.js"
+]
+
 // use the self keyword to instantiate listeners on the service worker.
 // The context of self here refers to the service worker object.
 self.addEventListener('install', function (e) {
@@ -13,13 +23,6 @@ self.addEventListener('install', function (e) {
     )
 })
 
-const FILES_TO_CACHE = [
-    "./models/transaction.js",
-    "./public/css/style.css",
-    "./public/js/idb.js",
-    "./public/js/index.js",
-    "./public/js/index.html",
-    "./routes/api.js",
-    "./server.js"
-]
+// self.addEventListener('activate', function (e) {
 
+// })
